a202c088948deaa81d5b9c97bde68e2a55f2d160
ekemini_udofia <ekeminiabasiudofia@gmail.com>
2025-02-10 10:24:42 +0100

    Finished within 1 hour of starting!

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <cstdlib>
-#include <cstdio>
 
 
-char square[10] = { '0', '1', '2', '3', '5', '6', '7', '8', '9' };
+
+char square[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 
 int check_for_win()
@@ -54,9 +54,9 @@ int check_for_win()
 void create_board()
 {
 	system("cls");
-	std::cout << "\n\n\t A  simple Tictactoe Console Game \n\n";
+	std::cout << "\n\n A  simple Tictactoe Console Game \n\n";
 
-	std::cout << "The first player X and the second is O \n\n";
+	std::cout << "The first player is X and the second is O \n\n";
 	std::cout << std::endl;
 
 	std::cout << "   |   |   " << std::endl;
@@ -77,7 +77,75 @@ void create_board()
 
 int main(int argv, char* argc[])
 {
+	int player = 1, i, choice;
+	char mark;
 
+	do
+	{
+		create_board();
+		player = (player % 2) ? 1 : 2;
 
+		std::cout << "player" << player << ", enter the number: ";
+		std::cin >> choice;
+
+		mark = (player == 1) ? 'X' : 'O';
+		if (choice == 1 && square[1] == '1')
+		{
+			square[1] == mark;
+		}
+		else if (choice == 2 && square[2] == '2')
+		{
+			square[2] == mark;
+		}
+		else if (choice == 3 && square[3] == '3')
+		{
+			square[3] == mark;
+		}
+		else if (choice == 4 && square[4] == '4')
+		{
+			square[4] == mark;
+		}
+		else if (choice == 5 && square[5] == '5')
+		{
+			square[5] == mark;
+		}
+		else if (choice == 6 && square[6] == '6')
+		{
+			square[6] == mark;
+		}
+		else if (choice == 7 && square[7] == '7')
+		{
+			square[7] == mark;
+		}
+		else if (choice == 8 && square[8] == '8')
+		{
+			square[8] == mark;
+		}
+		else if (choice == 9 && square[9] == '9')
+		{
+			square[9] == mark;
+		}
+		else
+		{
+			std::cout << "Move is Invalid\n";
+			player--;
+			std::cin.ignore();
+			std::cin.get();
+		}
+
+		i = check_for_win();
+		player++;
+	} while (i == -1);
+	create_board();
+	if (i == 1)
+	{
+		std::cout << "\n Conragts, Player " << player << " has won";
+	}
+	else
+	{
+		std::cout << "\n Draw";
+	}
+	std::cin.ignore();
+	std::cin.get();
 	return 0;
 }
